663f205755a4281ad60f927bdbf3602928058a51
varadpundlik <varadpundlik@gmail.com>
2024-02-25 21:32:00 +0530

    fix: constants

--- constant/index.js
+++ constant/index.js
@@ -2,7 +2,7 @@ const constant = {
   message: "Hello World",
 
   PATIENT_NOT_FOUND: "Patient not found",
-  PATIENT_CREATED: "PAtient created successfully",
+  PATIENT_CREATED: "Patient created successfully",
   PATIENT_UPDATED: "Patient updated successfully",
   PATIENT_DELETED: "Patient deleted successfully",
 };
