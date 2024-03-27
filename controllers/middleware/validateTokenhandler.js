8f68a6d9f8276b005ff8b4c90aa5feebfac9fff8
Pranay Agrawal <agrawalpranay070@gmail.com>
2024-03-27 19:02:06 +0530

    new file:   controllers/middleware/validateTokenhandler.js 	modified:   controllers/user.js

---
+++ controllers/middleware/validateTokenhandler.js
@@ -0,0 +1,26 @@
+const asyncHandler = require("express-async-handler");
+const config = require("../config");
+const jwt = require("jsonwebtoken");
+
+const validateToken = async (req, res, next) => {
+  let token;
+  let authHeader = req.headers.Authorization || req.headers.authorization;
+  if (authHeader && authHeader.startsWith("Bearer")) {
+    token = authHeader.split(" ")[1];
+    jwt.verify(token, config.Access_token_secret, (err, decoded) => {
+      if (err) {
+        res.status(401);
+        throw new Error("User is not authorized");
+      }
+      req.user = decoded.user;
+      next();
+    });
+
+    if (!token) {
+      res.status(401);
+      throw new Error("User is not authorized or token is missing");
+    }
+  }
+};
+
+module.exports = validateToken;                                                                                     
